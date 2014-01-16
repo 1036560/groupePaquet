62bc863ca3438deda77925594b09334bed644514
alexandrepl <alexandre.picardlemieux@hotmail.com>
2014-01-16 13:28:32 -0500

    modif de la page

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
       <li class="name">
         <h1>
           <a href="#">
-            Top Bar Title
+            Groupe Paquet
           </a>
         </h1>
       </li>
@@ -38,7 +38,7 @@
       <ul class="right">
         <li class="divider"></li>
         <li class="has-dropdown">
-          <a href="#">Main Item 1</a>
+          <a href="#">Accueil</a>
           <ul class="dropdown">
             <li><label>Section Name</label></li>
             <li class="has-dropdown">
@@ -64,10 +64,10 @@
           </ul>
         </li>
         <li class="divider"></li>
-        <li><a href="#">Main Item 2</a></li>
+        <li><a href="#">Demande d'emploi</a></li>
         <li class="divider"></li>
         <li class="has-dropdown">
-          <a href="#">Main Item 3</a>
+          <a href="#">Demande de commandite</a>
           <ul class="dropdown">
             <li><a href="#">Dropdown Option</a></li>
             <li><a href="#">Dropdown Option</a></li>
@@ -118,100 +118,46 @@
         <div class="large-12 columns">
           <div class="row">
             <!-- Shows -->
-            <div class="large-4 small-6 columns">
- 
-              <h4>Upcoming Shows</h4><hr>
- 
-              <div class="row">
-                <div class="large-1 column">
-                  <img src="http://placehold.it/50x50&text=[img]">
-                </div>
- 
-                <div class="large-9 columns">
-                  <h5><a href="#">Venue Name</a></h5>
-                  <h6 class="subheader show-for-small">Doors at 00:00pm</h6>
-                </div>
-              </div><hr>
- 
-              <div class="hide-for-small">
-                <div class="row">
-                  <div class="large-1 column">
-                    <img src="http://placehold.it/50x50&text=[img]">
-                  </div>
- 
-                  <div class="large-9 columns">
-                    <h5 class="subheader"><a href="#">Venue Name</a></h5>
-                  </div>
-                </div><hr>
- 
-                <div class="row">
-                  <div class="large-1 column">
-                    <img src="http://placehold.it/50x50&text=[img]">
-                  </div>
- 
-                  <div class="large-9 columns">
-                    <h5 class="subheader"><a href="#">Venue Name</a></h5>
-                  </div>
-                </div><hr>
- 
-                <div class="row">
-                  <div class="large-1 column">
-                    <img src="http://placehold.it/50x50&text=[img]">
-                  </div>
- 
-                  <div class="large-9 columns">
-                    <h5 class="subheader"><a href="#">Venue Name</a></h5>
-                  </div>
-                </div>
-              </div>
-            </div>
             <!-- End Shows -->
  
  
             <!-- Image -->
  
-            <div class="large-4 small-6 columns">
-              <img src="http://placehold.it/300x465&text=Image">
-            </div>
+	            <div class="large-4 small-4 columns">
+	              <img src="http://placehold.it/300x465&text=Image">
+	            </div>
+	            <div class="large-4 small-4 columns">
+	              <img src="http://placehold.it/300x465&text=Image">
+	            </div>
+	            <div class="large-4 small-4 columns">
+	              <img src="http://placehold.it/300x465&text=Image">
+	            </div>
+           </div>
+        </div>
+        <hr />
+        <div class="large-12 columns">
+          <div class="row">
+            <!-- Shows -->
+            <!-- End Shows -->
  
-            <!-- End Image -->
  
+            <!-- Image -->
  
-            <!-- Feed -->
- 
-            <div class="large-4 small-12 columns">
- 
-              <h4>Blog</h4><hr>
-              <div class="panel">
-                <h5><a href="#">Post Title 1</a></h5>
- 
-                <h6 class="subheader">
-                  Risus ligula, aliquam nec fermentum vitae, sollicitudin eget urna. Suspendisse ultrices ornare tempor...
-                </h6>
- 
-                <h6><a href="#">Read More »</a></h6>
-              </div>
- 
-              <div class="panel hide-for-small">
-                <h5><a href="#">Post Title 2 »</a></h5>
-              </div>
- 
-              <div class="panel hide-for-small">
-                <h5><a href="#">Post Title 3 »</a></h5>
-              </div>
- 
-              <a href="#" class="right">Go To Blog »</a>
- 
-            </div>
- 
-            <!-- End Feed -->
- 
-          </div>
+	            <div class="large-4 small-4 columns">
+	              <img src="http://placehold.it/300x465&text=Image">
+	            </div>
+	            <div class="large-4 small-4 columns">
+	              <img src="http://placehold.it/300x465&text=Image">
+	            </div>
+	            <div class="large-4 small-4 columns">
+	              <img src="http://placehold.it/300x465&text=Image">
+	            </div>
+           </div>
         </div>
       </div>
  
     <!-- End Content -->
- 
+
  
     <!-- Footer -->
  
